1ef0d5afef4ee316bd9753234f2c98a2f5009a95
luisginan <luis@sahabatbisnis.co.id>
2022-11-24 10:00:22 +0700

    add ubuntu script

---
+++ ubuntu/install-essential-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install stacer -y
+sudo apt install httpie -y
+sudo apt install git -y
+sudo apt install python3 -y
+sudo apt install nvm -y
+sudo apt install gimp -y
+sudo apt install inkscape -y
+sudo apt install dotnet7
+
+
+
+sudo apt install snapd -y
+sudo snap install postman 
+sudo snap install code --classic 
+sudo snapt install snap-store
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install dbeaver-ce
+
