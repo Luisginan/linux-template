e0f3b6646f5d318c54892618a95088a76ff6706d
luisginan <luis@sahabatbisnis.co.id>
2022-11-29 09:32:36 +0700

    add java jvm

--- ubuntu/install-essential-app.sh
+++ ubuntu/install-essential-app.sh
@@ -20,9 +20,22 @@ sudo apt install neofetch -y
 sudo apt install tilix -y
 sudo apt install htop -y
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+
+sudo apt install curl git
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+. $HOME/.asdf/asdf.sh
+asdf plugin-add java
+asdf install java oracle-19
+asdf global java oracle-19
+
+
 sudo apt install flatpak -y
 sudo add-apt-repository ppa:flatpak/stable
-
 
 sudo apt install snapd -y
 sudo snap install postman 
